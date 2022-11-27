ec03ca20787e122ea1e275a016e6acb5045641ef
jiwon <ljwon77@daum.net>
2022-11-27 19:47:25 +0900

    fix: 기능 관련 오타 수정

--- src/main/java/pairmatching/constant/FunctionOption.java
+++ src/main/java/pairmatching/constant/FunctionOption.java
@@ -1,7 +1,7 @@
 package pairmatching.constant;
 
 public enum FunctionOption {
-    OPTION1("1. 페어 매칭", "3")
+    OPTION1("1. 페어 매칭", "1")
     , OPTION2("2. 페어 조회", "2")
     , OPTION3("3. 페어 초기화", "3")
     , QUIT("Q. 종료", "Q")
