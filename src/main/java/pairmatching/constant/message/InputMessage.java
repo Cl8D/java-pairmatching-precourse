73379e70ecc293fa6dd9c55a060385551e1d4cb7
jiwon <ljwon77@daum.net>
2022-11-27 23:13:33 +0900

    fix: 입력 메시지에 대해 enum 대신 final class 사용으로 변경

--- src/main/java/pairmatching/constant/message/InputMessage.java
+++ src/main/java/pairmatching/constant/message/InputMessage.java
@@ -1,18 +1,23 @@
 package pairmatching.constant.message;
 
-public enum InputMessage {
-    INPUT("기능을 선택하세요.\n%s\n%s\n%s\n%s")
-    ,SELECT("과정, 레벨, 미션을 선택하세요.\n" +
-                   "ex) %s, %s, %s")
-    ;
+import pairmatching.domain.Mission;
 
-    private String message;
+import static pairmatching.constant.Option.*;
+import static pairmatching.domain.Course.BACKEND;
+import static pairmatching.domain.Level.LEVEL1;
 
-    InputMessage(String message) {
-        this.message = message;
+public final class InputMessage {
+
+    private static final String INPUT = "기능을 선택하세요.\n%s\n%s\n%s\n%s";
+
+    private static final String SELECT = "과정, 레벨, 미션을 선택하세요.\nex) %s, %s, %s";
+
+    public static String getOptionMessage() {
+        return String.format(INPUT, OPTION1.getOptionMsg(), OPTION2.getOptionMsg(), OPTION3.getOptionMsg(), QUIT.getOptionMsg());
     }
 
-    public String getMessage() {
-        return message;
+    public static String getSelectMessage() {
+        return String.format(SELECT, BACKEND.getValue(), LEVEL1.getMessage(), Mission.CAR.getValue());
     }
+
 }
