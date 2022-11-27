828e06cf5eeb578bbbacbabdaeb42d2d943b9b20
jiwon <ljwon77@daum.net>
2022-11-27 22:58:44 +0900

    feat: 레벨에 대한 도메인 및 toString을 통한 내용 출력 기능 추가

---
+++ src/main/java/pairmatching/domain/Level.java
@@ -0,0 +1,52 @@
+package pairmatching.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static pairmatching.domain.Mission.*;
+
+public enum Level {
+
+    LEVEL1("레벨1", List.of(CAR, LOTTO, BASEBALL)) {
+        @Override
+        public String toString() {
+            List<String> missionNames = LEVEL1.missions.stream().map(Mission::getValue).collect(Collectors.toList());
+            return "  - " + LEVEL1.message + ": " + String.join(" | ", missionNames) + "\n";
+        }
+    },
+    LEVEL2("레벨2", List.of(BUCKET, PAYMENT, SUBWAY)) {
+        @Override
+        public String toString() {
+            List<String> missionNames = LEVEL2.missions.stream().map(Mission::getValue).collect(Collectors.toList());
+            return "  - " + LEVEL2.message + ": " + String.join(" | ", missionNames) + "\n";
+        }
+    },
+    LEVEL3("레벨3", null) {
+        @Override
+        public String toString() {
+            return "  - " + LEVEL3.message + ": \n";
+        }
+    },
+    LEVEL4("레벨4",  List.of(IMPROVEMENT, DEPLOY)) {
+        @Override
+        public String toString() {
+            List<String> missionNames = LEVEL4.missions.stream().map(Mission::getValue).collect(Collectors.toList());
+            return "  - " + LEVEL4.message + ": " + String.join(" | ", missionNames)+ "\n";
+        }
+    },
+    LEVEL5("레벨5", null) {
+        @Override
+        public String toString() {
+            return "  - " + LEVEL5.message + ": \n";
+        }
+    };
+
+    private final String message;
+
+    private final List<Mission> missions;
+
+    Level(String message, List<Mission> missions) {
+        this.message = message;
+        this.missions = missions;
+    }
+}
